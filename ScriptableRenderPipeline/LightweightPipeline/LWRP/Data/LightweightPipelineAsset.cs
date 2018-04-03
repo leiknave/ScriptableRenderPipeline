5251b1c0dee50ea5f84ee8566b257e1f60ae4672
Aleksandr Kirillov <aleksandrk@unity3d.com>
2018-04-03 12:24:16 +0200

    Added a default value for the new property.

--- ScriptableRenderPipeline/LightweightPipeline/LWRP/Data/LightweightPipelineAsset.cs
+++ ScriptableRenderPipeline/LightweightPipeline/LWRP/Data/LightweightPipelineAsset.cs
@@ -67,7 +67,7 @@ namespace UnityEngine.Experimental.Rendering.LightweightPipeline
         [SerializeField] private ShadowCascades m_ShadowCascades = ShadowCascades.FOUR_CASCADES;
         [SerializeField] private float m_Cascade2Split = 0.25f;
         [SerializeField] private Vector3 m_Cascade4Split = new Vector3(0.067f, 0.2f, 0.467f);
-        [SerializeField] private bool m_PreferDepthPrepassOverCopy;
+        [SerializeField] private bool m_PreferDepthPrepassOverCopy = false;
 
         [SerializeField]
         private LightweightPipelineResources m_ResourcesAsset;
